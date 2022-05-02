cd8221afcf0a14735ba79c1fda265bba9efdbe41
Craig Hill <craigthomashill@gmail.com>
2022-05-02 15:39:27 +0100

    add bulletpoint

--- zettelkasten.py
+++ zettelkasten.py
@@ -6,10 +6,10 @@ new_content = [line.replace("\n\n", "\n") for line in content]
 with open("zettel-notes/zettel_50.md", "w+", encoding='utf-8') as f:
     counter = 1
     for line_num, line in enumerate(new_content, start=1):
-        if line_num % 50 == 0 or line_num == 1:
+        if line_num % 100 == 0 or line_num == 1:
             f.write(f"# {counter}\n")
             counter += 1
-        f.write(line)
+        f.write(f"* {line}")
 
     
 
