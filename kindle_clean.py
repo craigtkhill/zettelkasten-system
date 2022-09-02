c17435e264795912e8626a09dbef01474840711a
Craig <craigthomashill@gmail.com>
2022-09-02 17:47:06 +0100

    renamed file dir to work correctly

--- kindle_clean.py
+++ kindle_clean.py
@@ -3,7 +3,7 @@ from sys import argv
 quotes = {}
 book_quotes = []
 
-with open(f"kindle-notes/{argv[0]}.txt", "r", encoding="utf-8") as f:
+with open(f"kindle-notes/{argv[1]}.txt", "r", encoding="utf-8") as f:
     book_quotes.extend(line for line in f if not line.startswith(("-", "\n", "=")))
 for i in range(len(book_quotes)):
     if i % 2 == 0:
@@ -12,7 +12,7 @@ for i in range(len(book_quotes)):
     else:
         quotes[book_quotes[i - 1]].append(book_quotes[i])
 
-with open(f"../zettelkasten-notes/{argv[1]}.md", "w+", encoding="utf-8") as f:
+with open(f"zettel-notes/{argv[2]}.md", "w+", encoding="utf-8") as f:
     for book, quote in quotes.items():
         f.write(f"## {book.upper()}\n")
         for line in quote:
