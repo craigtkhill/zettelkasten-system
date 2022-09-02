fc36104a1fa4bc71fdd31f29f663fd0e23e789b6
Craig <craigthomashill@gmail.com>
2022-09-02 18:21:03 +0100

    make file names the same as current date

--- kindle_clean.py
+++ kindle_clean.py
@@ -1,9 +1,16 @@
 from sys import argv
+from datetime import datetime
 
 quotes = {}
 book_quotes = []
 
-with open(f"kindle-notes/{argv[1]}.txt", "r", encoding="utf-8") as f:
+# get the current date as a string
+def get_date():
+    return datetime.now().strftime("%Y-%m-%d")
+
+date = get_date()
+
+with open(f"kindle-notes/{date}.txt", "r", encoding="utf-8") as f:
     book_quotes.extend(line for line in f if not line.startswith(("-", "\n", "=")))
 for i in range(len(book_quotes)):
     if i % 2 == 0:
@@ -12,7 +19,7 @@ for i in range(len(book_quotes)):
     else:
         quotes[book_quotes[i - 1]].append(book_quotes[i])
 
-with open(f"zettel-notes/{argv[2]}.md", "w+", encoding="utf-8") as f:
+with open(f"zettel-notes/{date}.md", "w+", encoding="utf-8") as f:
     for book, quote in quotes.items():
         f.write(f"## {book.upper()}\n")
         for line in quote:
